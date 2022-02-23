a80731eafd13c9660f38a1b4c80f3b057577e5f4
Harun <harun.r@bitmascot.com>
2022-02-23 20:04:07 +0600

    our-fleet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,44 @@
 // Mobile Nav
-$(".show-nav").click(function(){
-    $(".header-right").slideToggle(500);
-    $(".mobile-icon").text() === 'close'
-    ? $(".mobile-icon").text('menu')
-    : $(".mobile-icon").text('close')
-    return false;
+$(".show-nav").click(function () {
+  $(".header-right").slideToggle(500);
+  $(".mobile-icon").text() === "close"
+    ? $(".mobile-icon").text("menu")
+    : $(".mobile-icon").text("close");
+  return false;
 });
 
-// Hero Slider
-$('#hero-slider').owlCarousel({
-    loop:true,
-    nav:false,
-    dots:true,
-    items:1
-});
+// Date Picker
 $(".pick-up-date-time").datetimepicker();
 $(".drop-of-date-time").datetimepicker();
+
+// Hero Slider
+$("#hero-slider").owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  items: 1
+});
+// Hero Slider
+$("#our-fleet-slider").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  dots: false,
+  navText: [
+    '<span class="material-icons">chevron_left</span>',
+    '<span class="material-icons">chevron_right</span>'
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      margin: 10
+    },
+    991: {
+      items: 1,
+      margin: 10
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
